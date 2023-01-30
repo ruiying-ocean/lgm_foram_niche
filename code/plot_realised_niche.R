d8ca69afcf92de8207032451fcc870ff7b3623ac
ruiying-ocean <38958822+ruiying-ocean@users.noreply.github.com>
2023-01-30 23:03:21 +0000

    My own way to tag subplot

--- code/plot_realised_niche.R
+++ code/plot_realised_niche.R
@@ -5,7 +5,6 @@ library(geometry)
 ## library(wesanderson)
 library(pals)
 library(ggsci)
-library(egg)
 
 df_modern <- read_csv("data/modern_4pca.csv") %>% select(-1) %>% mutate(age  = "modern") %>% mutate(age = factor(age, levels=c("modern", "future", "LGM")))
 df_lgm <- read_csv("data/lgm_4pca.csv") %>% select(-1)  %>% mutate(age  = "LGM")  %>% mutate(age = factor(age, levels=c("modern", "future", "LGM")))
@@ -24,7 +23,7 @@ foram_names <- c(
 
 p1 <- db %>% ggplot(aes(x=sst, y=biomass, color=age,fill=age, group=age)) +
   geom_point(alpha=0.3)+
-  geom_smooth(method="loess",se=T, fullrange=F) +
+  geom_smooth(method="loess",se=T, fullrange=F)+
   facet_wrap(~foram,  strip.position = "bottom", labeller = as_labeller(foram_names)) +
   xlab("SST (°C)")+ 
   ylab(expression(Log[10]~"biomass ("*Log[10]~"mmol C"~m^-3*")"))+ 
@@ -46,9 +45,17 @@ p2 <- db %>% ggplot(aes(x=prey, y=biomass, color=age,fill=age, group=age)) +
   theme(text=element_text(family="Fira Sans"))+
   theme(strip.placement = "outside", strip.background = element_blank())
 
-p1 <- tag_facet(p1)
-p2 <- tag_facet(p2)
+## labelling sub-plots
+text_data <- data.frame(
+  age = rep(c("modern","future","LGM"),4),
+  foram = rep(c("bn", "bs", "sn", "ss"), each=3),
+  label = rep(letters[1:4], each=3)
+)
+
+p1 <- p1 + geom_text(data=text_data, aes(label=label, group=age), inherit.aes = FALSE,
+                x = Inf, y = -Inf, hjust=2, vjust=-1, fontface = "bold", size = 6)
+p2 <- p2 + geom_text(data=text_data, aes(label=label, group=age), inherit.aes = FALSE,
+                      x = Inf, y = -Inf, hjust=2, vjust=-1, fontface = "bold", size=6)
 
 ggsave("output/realised_thermal_niche.jpg", p1, dpi=300)
 ggsave("output/realised_food_niche.jpg", p2, dpi=300)
-
