8c9bb6b58d2f456670d4da104c53155c49ca95fc
ruiying-ocean <38958822+ruiying-ocean@users.noreply.github.com>
2023-01-30 22:29:44 +0000

    rename and beautify the realised niche plot

--- code/plot_realised_niche.R
+++ code/plot_realised_niche.R
@@ -5,6 +5,7 @@ library(geometry)
 ## library(wesanderson)
 library(pals)
 library(ggsci)
+library(egg)
 
 df_modern <- read_csv("data/modern_4pca.csv") %>% select(-1) %>% mutate(age  = "modern") %>% mutate(age = factor(age, levels=c("modern", "future", "LGM")))
 df_lgm <- read_csv("data/lgm_4pca.csv") %>% select(-1)  %>% mutate(age  = "LGM")  %>% mutate(age = factor(age, levels=c("modern", "future", "LGM")))
@@ -14,33 +15,40 @@ remove("df_modern", "df_lgm", "df_future")
 
 db <- db %>% mutate(biomass=log(biomass)) %>% filter(biomass>-9)
 
+foram_names <- c(
+  `bn` = "symbiont-barren non-spinose",
+  `bs` = "symbiont-barren spinose",
+  `sn` = "symbiont-facultative non-spinose",
+  `ss` = "symbiont-obligate spinose"
+)
+
 p1 <- db %>% ggplot(aes(x=sst, y=biomass, color=age,fill=age, group=age)) +
   geom_point(alpha=0.3)+
   geom_smooth(method="gam", se=T, fullrange=F) +
-  facet_wrap(~foram) + theme_bw() + 
+  facet_wrap(~foram,  strip.position = "bottom", labeller = as_labeller(foram_names)) +
+  xlab("SST (°C)")+ 
+  ylab(expression(Log[10]~"biomass ("*Log[10]~"mmol C"~m^-3*")"))+ 
+  scale_color_manual(values=c("#3D3B25FF", "#C71000FF", "#008EA0FF")) + 
+  scale_fill_manual(values=c("#3D3B25FF", "#C71000FF", "#008EA0FF")) + 
+  theme_bw() + 
   theme(text=element_text(family="Fira Sans"))+
-  scale_color_jama() +  scale_fill_jama()
+  theme(strip.placement = "outside", strip.background = element_blank())
 
 p2 <- db %>% ggplot(aes(x=prey, y=biomass, color=age,fill=age, group=age)) +
   geom_point(alpha=0.3)+
   geom_smooth(method="gam", se=T, fullrange=F) +
-  facet_wrap(~foram) + theme_bw() + 
+  facet_wrap(~foram,  strip.position = "bottom", labeller = as_labeller(foram_names)) +
+  xlab(expression("Preferred prey biomass (mmol C"~m^-3*")"))+ 
+  ylab(expression(Log[10]~"biomass ("*Log[10]~"mmol C"~m^-3*")"))+ 
+  scale_color_manual(values=c("#3D3B25FF", "#C71000FF", "#008EA0FF")) + 
+  scale_fill_manual(values=c("#3D3B25FF", "#C71000FF", "#008EA0FF")) + 
+  theme_bw() + 
   theme(text=element_text(family="Fira Sans"))+
-  scale_color_jama() +  scale_fill_jama()
+  theme(strip.placement = "outside", strip.background = element_blank())
+
+p1 <- tag_facet(p1)
+p2 <- tag_facet(p2)
 
 ggsave("output/thermal_niche.jpg", p1, dpi=300)
 ggsave("output/food_niche.jpg", p2, dpi=300)
 
-db %>% ggplot(aes(x=log(po4), y=biomass, color=age,fill=age, group=age)) +
-  geom_point(alpha=0.3)+
-  geom_smooth(method="gam", se=T, fullrange=F) +
-  facet_wrap(~foram) + theme_bw() + 
-  theme(text=element_text(family="Fira Sans"))+
-  scale_color_jama() +  scale_fill_jama()
-
-db %>% ggplot(aes(x=lat, y=biomass, color=age,fill=age, group=age)) +
-  geom_point(alpha=0.3)+
-  geom_smooth(se=T, fullrange=F) +
-  facet_wrap(~foram) + theme_bw() + 
-  theme(text=element_text(family="Fira Sans"))+
-  scale_color_jama() +  scale_fill_jama()
