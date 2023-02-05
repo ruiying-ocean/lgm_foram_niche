ac96e7f7b681c9955c1d87325c207ad8b41dd6ed
ruiying-ocean <38958822+ruiying-ocean@users.noreply.github.com>
2023-02-05 13:20:36 +0000

    modified the niche_ecospat plot which now uses `prcomp`

--- code/plot_niche_ecospat.R
+++ code/plot_niche_ecospat.R
@@ -1,8 +1,6 @@
 library(ade4)
 library(tidyverse)
-library("ecospat")
-#library("explor")
-#explor(pca.env)
+library(ecospat)
 
 modern <- read_csv("data/modern_4pca.csv") %>% select(-1) %>% mutate(age  = "modern") %>% mutate(age = factor(age, levels=c("modern", "future", "LGM")))
 lgm <- read_csv("data/lgm_4pca.csv") %>% select(-1)  %>% mutate(age  = "LGM")  %>% mutate(age = factor(age, levels=c("modern", "future", "LGM")))
@@ -10,92 +8,139 @@ future <- read_csv("data/future4_4pca.csv") %>% select(-1) %>% mutate(age  = "fu
 
 all <- rbind(modern, lgm, future) %>% pivot_longer(cols=bn:ss, names_to = "foram", values_to="biomass")
 
+# perform PCA before scaling each column
 pca.env <- all %>% select(sst:stratification) %>%
-  dudi.pca(scannf = F, nf=2, center = T, scale=T)
+  prcomp(center=T, scale=T)
 
-#pca.env$li %>% ggplot() + geom_density_2d_filled(aes(x = Axis1, y=Axis1))
+# print loading
+print(pca.env$rotation)
 
-plot_niche_overlap <- function(pca, data1, data2, sp, title, age1, age2, col1, col2, col3="brown", tag="a", min_biomass=1E-9){
+# plot loading
+# library(explor)
+# explor(pca.env)
 
-  # variance explained (eigenvalue percentage)
-  explained_variance <- pca.env$eig/sum(pca.env$eig)
-  var_pc1 <- round(explained_variance[1] * 100, 2)
-  var_pc2 <- round(explained_variance[2] * 100, 2)
 
-  # loadings
-  print(pca.env$c1)
+plot_niche_overlap <- function(pca, age1, age2, foram_sp,
+                               title="", 
+                               col1="blue", col2="red", col3="purple", 
+                               tag="a", foram_threshold=1E-9){
+  ## scores, check the table in below link to know loading/scores output
+  ## https://aedin.github.io/PCAworkshop/articles/b_PCA.html#difference-between-covariance-based-and-correlation-based-pca-1
+  scores.all.ages <- pca$x[, 1:2]
+  colnames(scores.all.ages) <- c("PC1", "PC2")
+
+  ## combine the score data with the original table
+  comb.data <- cbind(all, scores.all.ages) %>% tibble()
+
+  ## subset scores
+  scores.sp.age1 <- comb.data %>% filter(age==age1 & foram==foram_sp) %>%
+    filter(biomass > foram_threshold) %>% select(c(PC1, PC2))
   
-  # score values for all ages
-  scores.all.ages <- pca.env$li
+  scores.sp.age2 <- comb.data %>% filter(age==age2 & foram==foram_sp) %>% 
+    filter(biomass > foram_threshold) %>% select(c(PC1, PC2))
 
-  # score values for each age
-  scores.sp.age1 <- suprow(pca.env, data1[which(data2[,sp]>min_biomass), 5:12])$li
-  scores.sp.age2 <- suprow(pca.env, data2[which(data2[,sp]>min_biomass), 5:12])$li
-  
-  # PCA axis value for the whole climate
-  scores.clim.age1 <- suprow(pca.env, data1[,5:12])$li
-  scores.clim.age2 <- suprow(pca.env, data2[,5:12])$li
+  ## PCA scores for the whole native study area
+  scores.clim.age1 <- comb.data %>% filter(age==age1) %>% select(c(PC1, PC2))
+  scores.clim.age2 <- comb.data %>% filter(age==age2) %>% select(c(PC1, PC2))
+
   
   # create grids with occurrence densities along environmental gradients.
-  grid.clim.age1 <- ecospat.grid.clim.dyn(glob=scores.all.ages,
-                                            glob1=scores.clim.age1,
-                                            sp=scores.sp.age1, R=100)
-
-   grid.clim.age2 <- ecospat.grid.clim.dyn(glob=scores.all.ages,
+  extend_range <- c(0,0,0,0)
+  grid.clim.age1 <- ecospat.grid.clim.dyn(glob=scores.all.ages, 
+                                          glob1=scores.clim.age1,
+                                          sp=scores.sp.age1,
+                                          R=120,
+                                          th.sp=0,
+                                          extend.extent=extend_range)
+  
+  grid.clim.age2 <- ecospat.grid.clim.dyn(glob=scores.all.ages, 
                                           glob1=scores.clim.age2,
-                                          sp=scores.sp.age2, R=100)
-
-  # niche stability in blue, niche expansion in red, and niche unfilling in blue
-  # shade: one climate's density
-  # stability: common niche
-  # expansion: climate 2's unique niche
-  # unfilling: climate 1's unique niche
-   p <- ecospat.plot.niche.dyn(grid.clim.age1, grid.clim.age2, quant=0, interest=2,
+                                          sp=scores.sp.age2,
+                                          R=120,
+                                          th.sp=0,
+                                          extend.extent=extend_range)   
+  
+  eigen_value <- pca$sdev^2
+  var_prop = eigen_value/sum(eigen_value)
+  var_pc1 = round(var_prop[1],3) * 100
+  var_pc2 = round(var_prop[2],3) * 100
+  
+  ## niche stability in blue, niche expansion in red, and niche unfilling in blue
+  ## shade: one climate's density
+  ## stability: common niche
+  ## expansion: climate 2's unique niche
+  ## unfilling: climate 1's unique niche
+  ecospat.plot.niche.dyn(grid.clim.age1,
+                         grid.clim.age2,
+                         quant=0, interest=2,
                           title= title,
                           name.axis1=paste0("PC1 (", var_pc1, "%)"),
                           name.axis2=paste0("PC2 (", var_pc2, "%)"),
                           col.stab = col3,
                           col.unf = col1, col.exp=col2,
-                          colZ1 =  col1, colZ2 = col2, transparency = 60)
-   overlap <- ecospat.niche.overlap(grid.clim.age1, grid.clim.age2, cor = F)$D
-   overlap <- round(overlap, 2)
-   overlap_label <- paste0("Niche overlap: ", overlap)
-   text(13, 3.5, overlap_label)
-   text(-10, 3.5, tag, font=2, cex=1.7)
-   legend(x = "bottomright", legend=c(age1, age2, "overlap"), lty = c(1, 1, 1), col = c(col1, col2, col3))
+                          colZ1 =  col1, colZ2 = col2,
+                         transparency = 60)
   
+  ## Schoener's D index
+  overlap <- ecospat.niche.overlap(grid.clim.age1, grid.clim.age2, cor = F)$D
+  overlap <- round(overlap, 2)
+  overlap_label <- paste0("Niche overlap: ", overlap)
+  
+  text(-13.5, 4, tag, font=2, cex=1.7)
+  text(-10, -6, overlap_label)
+  legend(x = "bottomright", legend=c(age1, age2, "overlap"), lty = c(1, 1, 1), col = c(col1, col2, col3))
 }
 
 pdf("output/niche_overlap.pdf",  width=8, height=10)
 par(mfrow = c(4, 2))
-plot_niche_overlap(pca.env,lgm, modern, 1, title = "symbiont-barren non-spinose",
-                   col1="#008EA0FF", col2="orange",
-                   age1="LGM", age2="Modern", tag="a")
 
-plot_niche_overlap(pca.env, modern, future, 1, title = "symbiont-barren non-spinose",
+plot_niche_overlap(pca.env, "LGM", "modern", 
+                   foram_sp="bn",
+                   title = "symbiont-barren non-spinose",
+                   col1="#008EA0FF", col2="orange",
+                   tag="a", col3="brown")
+
+plot_niche_overlap(pca.env, "modern", "future", 
+                   foram_sp="bn",
+                   title = "symbiont-barren non-spinose",
                    col1="orange", col2="#C71000FF",
-                   age1="Modern", age2="Future",  tag="b", col3="red")
+                   tag="b", col3="red")
 
-plot_niche_overlap(pca.env, lgm, modern, 2, title = "symbiont-barren spinose",
+plot_niche_overlap(pca.env, "LGM", "modern", 
+                   foram_sp="bs",
+                   title =  "symbiont-barren spinose",
                    col1="#008EA0FF", col2="orange",
-                   age1="LGM", age2="Modern",  tag="c")
-plot_niche_overlap(pca.env, modern, future, 2, title = "symbiont-barren spinose",
-                   col1="orange", col2="#C71000FF",col3="red",
-                   age1="Modern", age2="Future",  tag="d")
+                   tag="c", col3="brown")
 
-plot_niche_overlap(pca.env, lgm, modern, 3, title = "symbiont-facultative non-spinose",
-                   col1="#008EA0FF", col2="orange",
-                   age1="LGM", age2="Modern",  tag="e")
-plot_niche_overlap(pca.env, modern, future, 3, title = "symbiont-facultative non-spinose",
-                   col1="orange", col2="#C71000FF",col3="red",
-                   age1="Modern", age2="Future",  tag="f")
+plot_niche_overlap(pca.env, "modern", "future", 
+                   foram_sp="bs",
+                   title = "symbiont-barren spinose",
+                   col1="orange", col2="#C71000FF",
+                   tag="d", col3="red")
 
-plot_niche_overlap(pca.env, lgm, modern, 4, title = "symbiont-obligate spinose",
+plot_niche_overlap(pca.env, "LGM", "modern", 
+                   foram_sp="sn",
+                   title =  "symbiont-barren non-spinose",
                    col1="#008EA0FF", col2="orange",
-                   age1="LGM", age2="Modern",  tag="g")
-plot_niche_overlap(pca.env, modern, future, 4, title = "symbiont-obligate spinose",
-                   col1="orange", col2="#C71000FF",col3="red",
-                   age1="Modern", age2="Future",  tag="h")
+                   tag="e", col3="brown")
+
+plot_niche_overlap(pca.env, "modern", "future", 
+                   foram_sp="sn",
+                   title = "symbiont-barren non-spinose",
+                   col1="orange", col2="#C71000FF",
+                   tag="f", col3="red")
+
+plot_niche_overlap(pca.env, "LGM", "modern", 
+                   foram_sp="ss",
+                   title =  "symbiont-barren spinose",
+                   col1="#008EA0FF", col2="orange",
+                   tag="g", col3="brown")
+
+plot_niche_overlap(pca.env, "modern", "future", 
+                   foram_sp="ss",
+                   title = "symbiont-barren spinose",
+                   col1="orange", col2="#C71000FF",
+                   tag="h", col3="red")
 
 dev.off ()
 
