e0dfcac257bdd89bd0af7f4b85495fc9cb1cc7c8
Rui Ying (Mac) <38958822+ruiying-ocean@users.noreply.github.com>
2023-07-27 13:45:56 +0100

    A script to reproduce lombard 2009 model

--- code/lombard_2009_model.R
+++ code/lombard_2009_model.R
@@ -1,35 +1,47 @@
 library(data.table)
 library(tidyverse)
 
-ut1 = c(0.23, 0.3, 0.17, 0.24, 0.1, 0.25, 0.13, 0.37)
-t1 = 293 #20 C
-ta = c(626, 2155, 7105, 1086, 6876, 6482, 6584, 6584)
-# temperature lower bound (in Kel)
-tl = c(289.8, 289.3, 284.9, 292.3, 280.1, 281.1, 277.8, NA)
-# temperature upper bound (in Kel)
-th = c(304, 304.4, 301.8, 303.4, 298.8, 298.5, 293.7, 279.8)
-# Arrhenius temperatures
-tal = c(30953, 94385, 349991, 53765, 210746, 295374, 300239, NA)
-tah = c(249412, 171209,130852,165490,57855,159618,110583,59491)
 Species = c("O. universa",
             "G. sacculifer",
             "G. siphonifera",
-            "G. ruber w",
+            "G. ruber albus",
             "N. dutertrei",
             "G. bulloides",
             "N. incompta",
             "N. pachyderma"
-            )
-lombard_data <- data.table(Species, ut1, ta, tl, th,tal,tah) %>% t() %>%
-  as.data.table()
-names(lombard_data) <- as.character(lombard_data[1,])
-lombard_data <- lombard_data[-1, ]
-lombard_data <- sapply(lombard_data, function(x) as.numeric(as.character(x))) %>% as.data.frame()
-rownames(lombard_data) <- c("ut1", "ta", "tl", "th", "tal", "tah")
-lombard_data <- as_tibble(lombard_data)
-lombard_data%>%pivot_longer(`O. universa`:`N. pachyderma`, names_to = "Species", values_to = "Parameter_value")
+)
 
-SST = seq(0, 30, 0.1)
+ut1 = c(0.23, 0.30, 0.17, 0.24, 0.10, 0.25, 0.13, 0.37)
+ta = c(626, 2155, 7105, 1086, 6876, 6482, 6584, 6584)
+# temperature lower bound (in Kel)
+tl = c(289.8, 289.3, 284.9, 292.3, 280.1, 281.1, 277.8, 277.8 )
+# temperature upper bound (in Kel)
+th = c(304, 304.4, 301.8, 303.4, 298.8, 298.5, 293.7, 279.8)
+# Arrhenius temperatures
+tal = c(30953, 94385, 349991, 53765, 210746, 295374, 300239, 300239)
+tah = c(249412, 171209,130852,165490,57855,159618,110583,59491)
+lombard_lab <- data.table(Species, ut1, ta, tl, th,tal,tah) %>% t() %>%
+  as.data.table()
+
+
+ut1 = c(0.21, 0.29, 0.19, 0.19, 0.17, 0.31, 0.18, 0.19)
+kn = c(1.73, 1.32, 1.19, 0.51, 1.00, 6.84, 3.33, 4.70)
+tah = c(74, 313, 102000, 39284, 32319, 52575, 51836, 23802)
+th = c(305, 305, 302, 303, 304, 299, 296, 281)
+tal = c(31002, 51870, 270000, 44807, 103000, 202000, 164000, 20900)
+tl = c(287, 289, 285, 291, 281, 281, 277, 260)
+ta = c(5598, 3523, 10427, 7852, 8536, 9006, 8347, 3287)
+lombard_foramclim <- data.table(Species, ut1, ta, tl, th,tal,tah,kn) %>% t() %>%
+  as.data.table()
+
+names(lombard_lab) <- as.character(lombard_lab[1,])
+lombard_lab <- lombard_lab[-1, ]
+lombard_lab <- sapply(lombard_lab, function(x) as.numeric(as.character(x))) %>% as.data.frame()
+rownames(lombard_lab) <- c("ut1", "ta", "tl", "th", "tal", "tah")
+lombard_lab <- as_tibble(lombard_lab)
+lombard_lab%>%pivot_longer(`O. universa`:`N. pachyderma`, names_to = "Species", values_to = "Parameter_value")
+
+SST = seq(0, 35, 0.1)
 
 growth_rate <- function(temperature, pars, t1=293) {
   temperature = temperature+273.5
@@ -46,15 +58,16 @@ growth_rate <- function(temperature, pars, t1=293) {
 }
 
 modern_niche <- bind_rows(
-data.frame(SST, Species="N. dutertrei", Count=growth_rate(SST, lombard_data$`N. dutertrei`)*1000),
-data.frame(SST, Species="G. ruber w", Count=growth_rate(SST, lombard_data$`G. ruber w`)*1000),
-data.frame(SST, Species="T. sacculifer", Count=growth_rate(SST, lombard_data$`G. sacculifer`)*200),
-data.frame(SST, Species="O. universa", Count=growth_rate(SST, lombard_data$`O. universa`)*150),
-data.frame(SST, Species="N. dutertrei", Count=growth_rate(SST, lombard_data$`N. dutertrei`)*1000),
-data.frame(SST, Species="N. pachyderma", Count=growth_rate(SST, lombard_data$`N. pachyderma`)*1000),
-data.frame(SST, Species="N. incompta", Count=growth_rate(SST, lombard_data$`N. incompta`)*4000),
-data.frame(SST, Species="G. bulloides", Count=growth_rate(SST, lombard_data$`G. bulloides`)*800),
+data.frame(SST, Species="N. dutertrei", u=growth_rate(SST, lombard_foramclim$`N. dutertrei`)),
+data.frame(SST, Species="G. ruber albus", u=growth_rate(SST, lombard_foramclim$`G. ruber albus`)),
+data.frame(SST, Species="T. sacculifer", u=growth_rate(SST, lombard_foramclim$`G. sacculifer`)),
+data.frame(SST, Species="O. universa", u=growth_rate(SST, lombard_foramclim$`O. universa`)),
+data.frame(SST, Species="N. dutertrei", u=growth_rate(SST, lombard_foramclim$`N. dutertrei`)),
+data.frame(SST, Species="N. pachyderma", u=growth_rate(SST, lombard_foramclim$`N. pachyderma`)),
+data.frame(SST, Species="N. incompta", u=growth_rate(SST, lombard_foramclim$`N. incompta`)),
+data.frame(SST, Species="G. bulloides", u=growth_rate(SST, lombard_foramclim$`G. bulloides`)),
 )
-modern_niche$age="modern"
-modern_niche$SSS = NA
 
+modern_niche <- modern_niche %>%  mutate(u = if_else(Species == "N. pachyderma" & SST < 5, NA, u))
+modern_niche$age="modern"
+modern_niche %>% ggplot(aes(x=SST,y=u)) + geom_line(aes(color=Species), linewidth=1) + ylim(c(0,0.5))
