fe84895cfde9fd9e64a78ef23d74277482acd470
Rui Ying (Mac) <38958822+ruiying-ocean@users.noreply.github.com>
2023-10-29 13:31:12 +0000

    save untracked data/plots

---
+++ code/plot_thermal_fitness.R
@@ -0,0 +1,92 @@
+## This script contains functions to plot Fig. 1 and Fig. S3
+
+## Contact: rui.ying@bristol.ac.uk
+
+## load visualisation packages
+library(showtext)
+library(ggrepel)
+library(patchwork)
+source("code/lib.R")
+
+## read presaved Rdata
+load("data/obs_smooth.Rdata")
+load("data/obs_raw.Rdata")
+
+# Set the global theme for text properties
+###### Fig 1
+fig1a <- plot_tpc(raw_data = obs_fg_a_raw, smooth_data = obs_fg_a_smooth, x='sst', y='abundance')
+
+## change color and labels
+fig1a <- fig1a + labs(x="Sea surface temperature (°C)", y="Abundance (#)")
+
+fig1a+theme_publication()
+## do the same for GENIE model output
+## subset the LGM and PI data
+fig1b <- ggplot() + geom_point(data=genie_fg_raw %>% filter(age=="lgm" | age=="pi"), aes(x=sst, y =abundance_michaels, color=age), size=1, alpha=0.2)
+fig1b <- fig1b + geom_line(data=genie_fg_smooth %>% filter(age=="lgm" | age=="pi"), aes(x=model_x, y =model_y, color=age),  linewidth=1)
+fig1b <- fig1b + facet_wrap(~species, scale="free_y",nrow=1)
+
+fig1b <- fig1b  + scale_color_manual(values=c(c_cold,c_warm), labels=c("LGM","Pre-industrial")) +
+  labs(x="Sea surface temperature (°C)",
+       y = expression("Abundance (" * "#/m"^3 * ")"))
+
+fig1b <- fig1b + theme(legend.position = "none")
+fig1b <- fig1b + geom_vline(data = thermal_opt(genie_fg_smooth %>% filter(age=="lgm" | age=="pi")), aes(xintercept = model_x, color = age), linetype = "dashed", linewidth=0.5)
+fig1b <- fig1b + scale_y_continuous(limits = ~ c(min(.x), ceiling(max(.x)))*1.5)
+fig1b <- fig1b + geom_label_repel(data = thermal_opt(genie_fg_smooth %>% filter(age=="lgm" | age=="pi")),
+                                aes(x = model_x, y=model_y, fill = age, label=round(model_x)),
+                                color="white",
+                                size=4,
+                                  nudge_y = 30, label.r = 0.05, label.size=0.1)+
+  scale_fill_manual(values=c(c_cold, c_warm))
+#fig1b <- fig1b + annotate(geom = 'text', label = 'Model', x = Inf, y = Inf,
+#                          vjust = 1.5, hjust=1.1,fontface ="italic", size=4)
+
+fig1a <- fig1a+ggtitle("Fossil Observation") + xlim(-2,32)+theme(plot.tag = element_text(face = 'bold'))
+fig1b <- fig1b+ggtitle("ForamEcoGENIE Model")+xlim(-2,32)+theme(plot.tag = element_text(face = 'bold'))
+
+fig1a<-fig1a+theme(panel.border = element_rect(colour = "black", fill=NA, linewidth=0.8))  
+fig1b<-fig1b+theme(panel.border = element_rect(colour = "black", fill=NA, linewidth=0.8))  
+fig1 <- fig1b/fig1a+ plot_annotation(tag_levels = 'a') 
+fig1 %>% ggsave(., file="output/fig1.pdf", dpi=400, width=10, height = 6)
+
+
+figs3 <- plot_tpc(obs_sp_raw, obs_sp_smooth, x="SST", y = "Abundance")
+figs3 <- figs3  + theme(strip.text = element_text(face = "italic"), legend.position = "none")
+figs3%>%ggsave(file = "output/figs3.jpg", dpi = 400, width = 12, height = 8)
+
+### Fig2b
+## the same color as the python script
+color_palette <- c(c_cold, c_warm, '#420a68', '#932667', '#dd513a', '#fca50a')
+
+genie_fg_smooth$age <- factor(genie_fg_smooth$age, levels = c("lgm","pi","historical","future1p5","future2","future3","future4"))
+
+fig2b <- genie_fg_smooth %>% filter(age!="historical")%>%
+  ggplot(aes(x=model_x, y=model_y, color=age))+
+  geom_line(linewidth=1) +
+  facet_wrap(~species,scales = "free_y", nrow=1) +
+  labs(x="Sea surface temperature (°C)",
+       y = expression("Abundance (" * "#/m"^3 * ")"),  # LaTeX expression for the y-axis label
+       color = "")
+
+fig2b <- fig2b+ 
+  theme(legend.position = "none",
+        panel.border = element_rect(colour = "black", fill=NA, linewidth=0.8),
+        axis.text.y = element_text(size = 8),  # Adjust the font size here (smaller value)
+        strip.text = element_text(size = 12),
+        strip.background = element_blank()) + 
+  scale_color_manual(values = color_palette, 
+                     labels = c("Last Glacial Maximum", "Pre-industrial", "2100 (+1°C)", "2100 (+2°C)", "2100 (+3°C)", "2100 (+4°C)"))
+
+ggsave("output/fig2b.jpg", width=9, height=2.5, dpi=300)
+
+## as fig1 but plot chl
+figs5 <- ggplot() + geom_line(data=genie_fg_smooth_chl%>% filter(age!="historical"), aes(x=model_x, y =model_y, color=age),  linewidth=1)
+figs5 <- figs5 + facet_wrap(~species, scale="free_y",nrow=1)
+figs5 <- figs5  + 
+  scale_color_manual(values = color_palette, 
+                     labels = c("Last Glacial Maximum", "Pre-industrial", "2100 (+1.5°C)", "2100 (+2°C)", "2100 (+3°C)", "2100 (+4°C)"))+
+    labs(x=expression("Total Chlorophyll (" * "mg/m"^3 * ")"),
+       y = expression("Abundance (" * "#/m"^3 * ")"))
+figs5 <- figs5 +  theme(legend.position = "bottom")
+ggsave("output/figs5.jpg",figs5, width=8, height=4, dpi=300)
