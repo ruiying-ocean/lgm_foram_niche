aba91241881dacb6fb8740f006e6a52518ab490d
Rui Ying (Mac) <38958822+ruiying-ocean@users.noreply.github.com>
2023-11-15 11:40:24 +0000

    update foram_sp_db to foram_taxonomy

--- code/anova_analysis.R
+++ code/anova_analysis.R
@@ -15,10 +15,10 @@ Topt_diff <- obs_sp_smooth %>% thermal_opt(long_format=F) %>% mutate(diff = PI-L
 write_csv(Topt_diff, "data/Topt_sp_lgm.csv")
 
 ## merge with species trait data table
-foram_sp_db <- read_csv("https://raw.githubusercontent.com/ruiying-ocean/lgm_foram_census/main/fg/foram_sp_db.csv")
+foram_sp_db <- read_csv("https://raw.githubusercontent.com/ruiying-ocean/lgm_foram_census/main/fg/foram_taxonomy.csv")
 
 trait_info <-foram_sp_db %>%
-   mutate(sp = map_vec(Name, species_abbrev)) %>% select(sp, Symbiosis, Spinose)
+   mutate(sp = map_vec(`Species name`, species_abbrev)) %>% select(sp, Symbiosis, Spine)
 
 ## check all species are included
 common_sp <- intersect(Topt_diff$species, trait_info$sp)
@@ -28,15 +28,14 @@ if (length(common_sp) == length(Topt_diff$species)) {
 
 Topt_diff <- Topt_diff %>% rename(sp=species) %>% left_join(trait_info, by = "sp") %>% distinct()
 
-Topt_diff <- Topt_diff %>% mutate(ecogroup = case_when(
-  Symbiosis == "Yes" & Spinose == "Yes"~ "Symbiont-obligate Spinose",
-  Symbiosis == "No" & Spinose == "Yes" ~ "Symbiont-barren Spinose",
-  Symbiosis == "No" & Spinose == "No" ~ "Symbiont-barren Non-spinose",
-  TRUE ~ "Symbiont-facultative Non-spinose")) %>%
-  filter(ecogroup != "Symbiont-facultative Non-spinose")
-
 ## anova analysis using symbiont and spine trait
-mod <- aov(diff ~ ecogroup, data = Topt_diff)
+Topt_diff <- Topt_diff %>% dplyr::filter(Symbiosis != 'undetermined',  Spine != 'underdetermined')
+
+
+## merge symbiont obligate and symbiont bearing groups
+## Topt_diff <- Topt_diff %>% mutate(Symbiosis = ifelse(Symbiosis == 'symbiont-obligate', 'symbiont-bearing', Symbiosis))
+
+mod <- aov(diff ~ Spine + Symbiosis, data = Topt_diff)
 summary(mod)
 
 ## comparison between groups
@@ -45,11 +44,12 @@ TukeyHSD(mod)
 ## ggpubr plot boxplot
 library(ggpubr)
 
-ggboxplot(Topt_diff, x='ecogroup',y='diff',fill='ecogroup',palette = "jco",add='jitter')+
+Topt_diff %>% mutate(ecogroup=paste(Symbiosis, Spine)) %>%
+    ggboxplot(, x='ecogroup',y='diff',fill='ecogroup',palette = "jco",add='jitter')+
   stat_compare_means(method = "anova",label = "p.signif") +
   theme_publication()+
   theme(legend.position = "none", axis.title.x = element_blank())+
   ylab(expression(paste("PI - LGM optimal temperature (",degree,"C)")))
 
 ## save the figure
-ggsave("output/figs10.png",width=8,height=4)
+ggsave("output/figs2.png",width=8,height=4)
--- code/plot_tpc.R
+++ code/plot_tpc.R
@@ -17,13 +17,16 @@ load("data/genie_fg_raw.Rdata")
 color_palette <- c("#0C4876", "#699c79", "#420a68", "#932667", "#dd513a", "#fca50a")
 
 ##### Fig 1a (model)
-fig1a <- plot_tpc(raw_data = genie_fg_raw %>% filter(age == "lgm" | age == "pi"), smooth_data = genie_fg_smooth %>% filter(age == "lgm" | age == "pi"), x = "sst", y = "abundance_michaels", vline = FALSE, colors = color_palette[1:2], labels = c("LGM", "PI"))
+fig1a <- plot_tpc(raw_data = genie_fg_raw %>% filter(age == "lgm" | age == "pi"), 
+                  smooth_data = genie_fg_smooth %>% filter(age == "lgm" | age == "pi"),
+                  x = "sst", y = "abundance_michaels", 
+                 colors = color_palette[1:2], labels = c("LGM", "PI"))
 
 ###### Fig 1b (data)
 ## not plotting two exceptional points in Symbiont-barren Spinose group
 fig1b <- plot_tpc(
   raw_data = obs_fg_a_raw %>% filter(species != "Symbiont-barren Spinose" | abundance < 600),
-  smooth_data = obs_fg_a_smooth, x = "sst", y = "abundance", vline = FALSE,
+  smooth_data = obs_fg_a_smooth, x = "sst", y = "abundance", vline = T,
   colors = color_palette[1:2], labels = c("LGM", "PI")
 )
 
@@ -41,27 +44,39 @@ fig1 <- wrap_plots(fig1a, fig1b, ncol = 1) %>% add_global_label(
   Xlab = "Annual mean sea surface temperature (°C)",
   fontface = "plain",
   size = 5
-)
+  )
+fig1
 
-fig1 %>% ggsave(., file = "output/fig1.png", dpi = 300, width = 10, height = 6)
+ggsave(file = "output/fig1.svg", fig1, dpi = 300, width = 10, height = 6)
+## convert to pdf
+## system("inkscape output/fig1.svg --export-pdf=output/fig1.pdf")
+## rsvg solution
+system("rsvg-convert -f pdf output/fig1.svg > output/fig1.pdf")
+## remove svg file
+system("rm output/fig1.svg")
 
 ## Fig. S3, species level thermal performance curves
 figs3 <- plot_tpc(obs_sp_raw, obs_sp_smooth, x = "SST", y = "Abundance", vline = FALSE, colors = color_palette[1:2], labels = c("LGM", "PI"))
 figs3 <- figs3 + labs(x = "Annual mean sea surface temperature (°C)", y = "Normalised abundance")
-figs3 <- figs3 + ylim(-0.1, 1)
+figs3 <- figs3 + ylim(-0.2, 1)
 figs3 <- figs3 + theme(strip.text = element_text(face = "italic"), legend.position = "none") + theme_publication(15)
 figs3 %>% ggsave(file = "output/figs3.jpg", dpi = 400, width = 12, height = 8)
 
 ### Fig2b, modelled thermal performance curves in the future
-genie_fg_smooth$age <- factor(genie_fg_smooth$age, levels = c("lgm", "pi", "historical", "future1p5", "future2", "future3", "future4"))
+genie_fg_smooth$age <- factor(genie_fg_smooth$age, levels = c("lgm", "pi", "historical", "future1p5", "future2", "future3", "future4", '3xCO2'))
 
 ## create an empty raw_data data.frame passing to plot_tpc
 ## abundance: 0, ages as in genie_fg_smooth, species as in genie_fg_smooth
-fake_df <- data.frame(abundance = 0, sst = 0, age = genie_fg_smooth$age, species = genie_fg_smooth$species) %>%
-  distinct() %>%
-  filter(age != "historical")
+fake_df <- data.frame(abundance = 0, sst = 0, age = genie_fg_smooth$age, species = genie_fg_smooth$species) %>% distinct()
 
-fig2b <- plot_tpc(raw_data = fake_df, smooth_data = filter(genie_fg_smooth, age != "historical"), x = "sst", y = "abundance", vline = FALSE, colors = color_palette, labels = c("LGM", "PI", "2100 (+1°C)", "2100 (+2°C)", "2100 (+3°C)", "2100 (+4°C)"), se = F)
+fig2b <- plot_tpc(raw_data = filter(fake_df, age != "historical" & age != "3xCO2"), 
+                  smooth_data = filter(genie_fg_smooth, age != "historical" & age != '3xCO2'), 
+                  x = "sst", 
+                  y = "abundance", 
+                  vline = F, 
+                  colors = color_palette, 
+                  labels = c("LGM", "PI", "2100 (+1°C)", "2100 (+2°C)", "2100 (+3°C)", "2100 (+4°C)"),
+                  se =F) 
 
 fig2b <- fig2b + labs(x = "Annual mean sea surface temperature (°C)", y = "Normalised abundance")
 
@@ -70,5 +85,17 @@ fig2b <- fig2b + theme_publication() +
       legend.position = "none",
   )
 
+filter(genie_fg_smooth, grepl("future", age)) %>% thermal_opt() %>% 
+  group_by(species)%>% summarise(future_mean = mean(opt_x_mean), future_sd= sd(opt_x_mean))
 
 ggsave("output/fig2b.jpg", width = 9, height = 2.8, dpi = 300)
+
+## plot a PI, future4, 3xPI comparison    
+fig_n <- plot_tpc(raw_data = filter(fake_df, age == 'pi' | age == '3xCO2'), 
+                  smooth_data = filter(genie_fg_smooth, age == "pi" | age == "3xCO2"),
+                  x = "sst", y = "abundance", vline = FALSE, se = TRUE)
+
+fig_n <- fig_n + labs(x = "Annual mean sea surface temperature (°C)", y = "Normalised abundance")
+fig_n <- fig_n + theme_publication() 
+fig_n
+ggsave("output/fig_n.jpg", width = 9, height = 2.8, dpi = 300)
